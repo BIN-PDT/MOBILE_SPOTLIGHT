00772011e886c5e5e4641dd902f0e5fc22e5fd9c
BIN BIN <thangpd300502@gmail.com>
2025-05-26 23:08:54 +0700

    AUTHENTICATION

---
+++ components/InitialLayout.tsx
@@ -0,0 +1,20 @@
+import { useAuth } from "@clerk/clerk-expo";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
+
+export default function InitialLayout() {
+	const router = useRouter();
+	const segments = useSegments();
+	const { isLoaded, isSignedIn } = useAuth();
+
+	useEffect(() => {
+		if (!isLoaded) return;
+
+		const inAuthScreen = segments[0] === "(auth)";
+		if (!isSignedIn && !inAuthScreen) router.replace("/(auth)/sign-in");
+		else if (isSignedIn && inAuthScreen) router.replace("/(tabs)");
+	}, [isLoaded, isSignedIn, segments]);
+
+	if (!isLoaded) return null;
+	return <Stack screenOptions={{ headerShown: false }} />;
+}
